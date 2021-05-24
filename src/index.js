d901b1b36887f408ecca1426bc3b3ec39298727d
suey0128 <syui0128@gmail.com>
2021-05-24 16:54:01 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// target the element we need to add the function to 
+addEventListener("DOMContentLoaded", (e) => { })
+//   // it is a form submission, we need to prevent default
+//   e.preventDefault();
+// });
+
+
+
+const newForm = document.querySelector('#create-task-form');
+let delBtn;
+let toDoli;
+
+function delLi() {
+  toDoli.remove();
+}
+
+newForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  toDoli = document.createElement('li');
+  toDoli.textContent = e.target['new-task-description'].value;
+  delBtn = document.createElement('button');
+  delBtn.textContent = 'x';
+  document.querySelector('#tasks').append(toDoli,delBtn); 
+  delBtn.onclick = "delLi()";
+})
+
+
+
+
+
+
+
+
+
+
+// const likeBtn = document.querySelector('#heart');
+// likeBtn.addEventListener('click',function like (e) {
+//     let likeLi = document.createElement('li');
+//     likeLi.textContent = `${counter.textContent} has been like 1 time`;
+//     document.querySelector('#likes').append(likeLi); 
+// }) 
+
+
+// document.querySelector('#comment-form').addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   let pComments = [];
+//   pComments.push(e.target.comment.value);
+//   pComments.forEach(elem => {
+//       let commentP = document.createElement('p')
+//       commentP.textContent = elem;
+//       document.querySelector('#list').append(commentP); 
+      
+//   })
+// })
